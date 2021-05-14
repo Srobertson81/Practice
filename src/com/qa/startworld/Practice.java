1ab383c28eeefe6daff0a80a9902424ef9a9e5e0
sybil_r <sybilwebdev@gmail.com>
2021-05-14 15:24:53 -0400

    java calculator project

---
+++ src/com/qa/startworld/Practice.java
@@ -0,0 +1,29 @@
+package com.qa.startworld;
+
+public class Practice {
+
+//	public static void main(String[] args) {
+//		
+//		int aNumber = 68;
+//		System.out.println(aNumber);
+//		char firstCharacter = 'Z';
+//		System.out.println(firstCharacter);
+//		String newString = "I will succeed!!!";
+//		System.out.println(newString);
+//		
+//		
+//
+//	}
+//	public static int add(int num1, int num2) {
+//		num1 = 1;
+//		num2 = 2;
+//		int result = num1 + num2;
+//		return add;
+////		System.out.println(result);
+//		
+//
+//	}
+//	
+
+
+}
